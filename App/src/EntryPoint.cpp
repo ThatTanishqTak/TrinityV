1ee132855a55061d3600d8936f24c63b05bdcd92
ThatTanishqTak <tanishqtak3@gmail.com>
2025-03-21 23:53:10 +0000

    First Window

---
+++ App/src/EntryPoint.cpp
@@ -0,0 +1,9 @@
+#include <../Engine/src/Engine.h>
+
+int main()
+{
+	Engine::Engine engine;
+	engine.Run();
+
+	return 0;
+}
---
+++ Engine/src/Engine.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+
+#include <iostream>
+
+namespace Engine
+{
+	class Engine
+	{
+	public:
+		void Run();
+	};
+}
